3ea06fcac9e1a9c1e741983d9e1e6cb4ff24f1e1
Murali Devi <profdevi@ymail.com>
2015-05-11 08:29:43 -0400

    Wrote Clean PrimeGenerator with UnitTest
    1.  Wrote a clean single class of prime generator
    
    2.  Added a unit test for PrimeGenerator as used by
        microsoft.

---
+++ PrimeGenerator.cs
@@ -0,0 +1,62 @@
+﻿//v0.1 copyright ?????? 20150511M0650
+using System.Collections.Generic;
+
+/////////////////////////////////////////////
+public class PrimeGenerator
+	{
+
+	//////////////////////////////////////////
+	// Check if number n is prime
+	public bool IsPrime(int n)
+		{
+		if(n<2) { return false; }
+
+		for(int k=2;k<n;++k)
+			{
+			if(n % k == 0) { return false; }
+			}
+
+		return true;
+		}
+
+	
+	//////////////////////////////////////////
+	// Print out the first n primes
+	public void PrintPrimes(int n)
+		{
+		int primecount=0;
+		for(int i=0;i>0;++i)
+			{
+			if(IsPrime(i)==false) { continue; }
+
+			primecount = primecount + 1;
+			System.Console.Write(" {0} ",i);
+
+			if(primecount>=n) { break; }
+			}
+
+		System.Console.WriteLine();
+		}
+
+
+	///////////////////////////////////////////
+	public List<int> GetPrimes(int n)
+		{
+		// Return the first n primes
+		return null;
+		}
+
+
+	//////////////////////////////////////////
+	public PrimeGenerator()
+		{
+		// Nothing Here Yet
+		}
+
+	/////////////////////////////////////////
+	~PrimeGenerator()
+		{
+		// Nothing Here Yet
+		}
+	}
+
---
+++ UnitTest/PrimeGeneratorTest.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+[TestClass]
+public class PrimeGeneratorTest
+	{
+	[TestMethod]
+	public void TestMethod1()
+		{
+		PrimeGenerator gen=new PrimeGenerator();
+		
+		// Check if primes works
+		int[] primes={2,3,5,7,11,13,17,19};
+		
+		for(int i=0;i<primes.Length;++i)
+			{
+			if(gen.IsPrime(primes[i])==false)
+				{
+				throw new Exception("Number should be a prime");
+				}
+			}
+
+		// check if nonprimes work		
+		int[] notprimes={0,1,4,6,8,9,10,12,14,15,16,18};
+		for(int i=0;i<primes.Length;++i)
+			{
+			if(gen.IsPrime(notprimes[i])==true)
+				{
+				throw new Exception("Number should not be a prime");
+				}
+			}
+		}
+	}
