ad446471ff8139cf38ad73c4665ae655bbf2e3ee
Murali Devi <profdevi@ymail.com>
2015-05-15 11:00:17 -0400

    Added Class NumberOps class

---
+++ NumberOps.cs
@@ -0,0 +1,32 @@
+﻿//v0.1 copyright ?????? 20150515F1054
+using System.Collections.Generic;
+
+////////////////////////////////////////////////
+class NumberOps
+	{
+	/////////////////////////////////
+	public NumberOps()
+		{
+		// Nothing yet
+		}
+
+	////////////////////////////////
+	~NumberOps()
+		{
+		// Nothing yet
+		}
+
+
+	/// <summary>
+	/// This Function will return the digits of a number in a list
+	/// </summary>
+	/// <param name="number">the number is positive</param>
+	/// <returns>List of digits</returns>
+	List<int> GetDigits(int number)
+		{
+		// Fill code to return a list of digits 
+		// For instance  the digit 1234 should return the list 1->2->3->4.
+		// Do not work about non positive numbers!
+		return null;
+		}
+	}
