2311d08816147ebb04389cde476ee9e60fa322b5
ramlall <tariqramlall@gmail.com>
2015-05-15 13:28:48 -0400

    Filled in the code for NumberOps to find digits in a number.
    Also added lines in Main to make use of the new NumberOps class.

--- NumberOps.cs
+++ NumberOps.cs
@@ -22,11 +22,63 @@ class NumberOps
 	/// </summary>
 	/// <param name="number">the number is positive</param>
 	/// <returns>List of digits</returns>
+	
+	///////////////////////////////////////////////////////
+	// Find the number of digits in the number.  
+	public int NumberOfDigits(int number)
+		{ 
+		string myString = number.ToString();
+		int DigitCount = myString.Length;
+
+		return DigitCount;
+		}
+
+
+	///////////////////////////////////////////////////////
+	// Get an int value for 10 raised to a power.
+	public int PowerOfTen(int i)
+		{ 
+		int power = 1;
+		for (int k = 1; k <= i; k++)
+			{ 
+			power = power * 10;
+			}
+		return power;
+		}
+
+
+	///////////////////////////////////////////////////////
+	// Return a list of the digits. 
 	List<int> GetDigits(int number)
 		{
 		// Fill code to return a list of digits 
 		// For instance  the digit 1234 should return the list 1->2->3->4.
 		// Do not work about non positive numbers!
-		return null;
+
+		List<int> Digits = new List<int>(); // List we'll be returning.
+		
+		int DigitCount = NumberOfDigits(number); // Check how many digits are in the number.
+
+		// Digit = [ number % (10^(the digits place)) ] / (10 ^ (the digits place - 1))
+		// The digits place = 1 if it's all the way on the right, 2 if it's in the 10s place, 3 if it's in the hundreds, etc.
+
+		for(int k = 1; k <= DigitCount; k++)
+			{
+			// Can't use System.Math.Pow for the exponents because the number would have to be a double. 
+			int digit = ( number % PowerOfTen(k) ) / PowerOfTen(k-1);
+			Digits.Add(digit);
+			}
+
+		return Digits;
+		}
+
+
+	//////////////////////////////////////////////////////
+	public void PrintDigits(int number)
+		{ 
+		List<int> Digits = GetDigits(number);
+		System.Console.WriteLine("The digits in {0} are:", number);
+		for (int k = Digits.Count - 1; k >= 0; k--) // For loop is in reverse order so the digits come out first to last.
+			{ System.Console.WriteLine(Digits[k]); }
 		}
 	}
--- Program.cs
+++ Program.cs
@@ -3,14 +3,28 @@ class Program
 	{
 	static void	Main(string[] args)
 		{
+		///////////// FOR PRIMES ////////////////////
 		// Ask user how many primes they want.
 		System.Console.WriteLine("Please input the number you want to check up too. (Must be above 3.)");
 		int prime = System.Convert.ToInt32(System.Console.ReadLine());
 
+		// For regular for loop Prime Generator.
 		//PrimeGenerator gen = new PrimeGenerator();
 		//gen.PrintPrimes(prime);	
 
+		// For Sieve Algorithm Generator
 		SieveGenerator sieve = new SieveGenerator();
 		sieve.PrintPrimes(prime); // Prime MUST be greater than 3 for this to work.
+
+		//////////////////////////////////////////////////////////
+
+
+		/////////////// FOR DIGITS /////////////////
+		System.Console.WriteLine("Please enter a number so we can break it down into it's digits.");
+		int digits = System.Convert.ToInt32(System.Console.ReadLine());
+
+		NumberOps num = new NumberOps();
+		num.PrintDigits(digits);
+		///////////////////////////////////////////////////////
 		}
     }
