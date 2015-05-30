92a01c6f883816f2c4dc667a1e00c8c0c76163fe
Steven Jelcic <stevenjelcic@gmail.com>
2015-05-30 10:29:17 -0400

    Added note for Tariq and Murali.
    Also made a few superficial changes.

--- Program.cs
+++ Program.cs
@@ -4,9 +4,10 @@ class Program
 	static void	Main(string[] args)
 		{
 		// Hi Steven.
+		// Hi Tariq and Murali; oh, and hello World!
 		///////////// FOR PRIMES ////////////////////
 		// Ask user how many primes they want.
-		System.Console.WriteLine("Please input the number you want to check up too. (Must be above 3.)");
+		System.Console.WriteLine("Please input the number you want to check up to. (Must be above 3.)");
 		int prime = System.Convert.ToInt32(System.Console.ReadLine());
 
 		// For regular for loop Prime Generator.
@@ -21,7 +22,7 @@ class Program
 
 
 		/////////////// FOR DIGITS /////////////////
-		System.Console.WriteLine("Please enter a number so we can break it down into it's digits.");
+		System.Console.WriteLine("Please enter a number so we can break it down into its digits.");
 		int digits = System.Convert.ToInt32(System.Console.ReadLine());
 
 		NumberOps num = new NumberOps();
